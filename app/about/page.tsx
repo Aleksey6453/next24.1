8a684b5bfc786e4b1b04ce2cdde923088cb82da5
Aleksey6453 <login6453@gmail.com>
2024-01-13 10:29:22 +0200

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'About page',
+    description: 'About page',
+  }
+
+export default function About(){
+    return (
+        <div>
+            <h1>
+                About
+            </h1>
+        </div>
+    )
+}
