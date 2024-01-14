51ac409a44c23f686afb5bb2c32082e97021a5b1
Aleksey6453 <login6453@gmail.com>
2024-01-14 21:11:30 +0200

    add dinamic blog with number of posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,37 @@
-export const metadata = {
-    title: 'Blog page',
-    description: 'Blog page',
-  }
+import {Metadata} from 'next'
+import Link from 'next/link'
+// export const metadata = {
+//     title: 'Blog page',
+//     description: 'Blog page',
+//   }
 
-export default function Blog(){
+async function getData(params:type) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    return response.json()
+}
+
+export const metadata: Metadata = {
+    ttitle: 'Blog | Next App'
+}
+
+export default async function Blog(){
+    const posts = await getData()
+
     return (
         <div className="wrapPage">
             <h1 className="title">
                Blog
             </h1>
+            <ul>
+                {posts.map((post: any) => (
+                    <li key={post.id}>
+                        <Link href={`/blog/${post.id}`}>
+                            Post №{post.id} {post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
