06ac22e3a8ec88e7d988824be3d69b608096ec41
Aleksey6453 <login6453@gmail.com>
2024-01-15 23:46:10 +0200

    add page of loading

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -25,6 +25,7 @@ export async function generateMetadata({params: {id}}: Props ){
 
 export default async function Post( {params: {id}} : Props ){
     const post = await getData(id)
+   
 
     return (
         <div className="wrapPage">
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading (){
+
+    return (
+        <div className="wrapPage">
+            <h1>Loading...</h1>
+            <img src="/loading.jpg" alt="loading" className="image"/>
+        </div>
+    )
+}
