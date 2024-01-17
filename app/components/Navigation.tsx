d251abd5b4bf7578402125d8415aab2d427c7c8c
Aleksey6453 <login6453@gmail.com>
2024-01-18 01:22:54 +0200

    add nav

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,17 @@
+'use client';
+import Link from "@/node_modules/next/link";
+import { usePathname } from "@/node_modules/next/navigation";
+
+
+
+const Navigation = ({}) => {
+    const pathname = usePathname()
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export {Navigation}
