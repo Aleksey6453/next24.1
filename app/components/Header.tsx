c72bcf6838fa0a27ca36ffda2e081a290c905fec
Aleksey6453 <login6453@gmail.com>
2024-01-22 22:56:20 +0200

    add dinamic style for navigation in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,20 @@
 import Link from "@/node_modules/next/link"
+import { Navigation } from "./Navigation"
+
+const navItems = [
+    {label: 'Home', href: '/'},
+    {label: 'Blog', href: '/blog'},
+    {label: 'About', href: '/about'},
+]
 
 const Header = () => {
     return (
         <header className='header'>
-            <Link href='/' className='linkHeader'>Home</Link>
+            {/* <Link href='/' className='linkHeader'>Home</Link>
             <Link href='/about' className='linkHeader'>About</Link>
-            <Link href='/blog' className='linkHeader'>Blog</Link>
+            <Link href='/blog' className='linkHeader'>Blog</Link> */}
+
+            <Navigation navLinks={navItems} />
         </header>
     )
 }
--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,17 +1,37 @@
 'use client';
 import Link from "@/node_modules/next/link";
 import { usePathname } from "@/node_modules/next/navigation";
+import { type } from "os";
+
+type NavLink = {
+    label: string,
+    href: string
+}
+
+type Props = {
+    navLinks: NavLink[]
+}
 
 
 
-const Navigation = ({}) => {
+const Navigation = ({navLinks}: Props) => {
     const pathname = usePathname()
 
   return (
-    <div>
-      
-    </div>
+    <>
+        {navLinks.map((link) => {
+        const isActive = pathname === link.href
+
+        return (
+            <Link key={link.label} href={link.href} className={isActive ? 'lime' : 'linkHeader'}>
+              {link.label}
+            </Link>
+        )
+        })}
+    </>
   )
+  
+  
 }
 
 export {Navigation}
